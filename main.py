52fcfdde8c2d6f9467b93b978d11920119a7e331
Kiran <kiransettyks@gmail.com>
2022-03-26 12:56:12 +0200

    Make the timer functional Wrap the paragraph correctly in window Have the stop/start button work correctly

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
 from random import choice
 from tkinter import *
+import textwrap
 
 sentences_file = open("sentences.txt", 'r')
 sentences = sentences_file.read().splitlines()
 sentences_file.close()
+timer = None
+start=True
 
 # Creating a new window and configurations
 window = Tk()
 window.title("Typing Speed Test")
-window.minsize(width=500, height=500)
+window.minsize(width=600, height=300)
 window.config(padx=10, pady=10, bg="#f7f5dd")  # Add padding to the edges of window
 
-paragraph = Label(text="Start the test to view the sentence to type", font=("Arial", 12, "bold"))
+paragraph = Label(text="Start the test to view the sentence to type", font=("Arial", 12))
 paragraph.pack()
 
-timer_tag = Label(text="0:00", font=("Arial", 10))
+
+def timer_count(m, s):
+    global timer
+    if s == 60:
+        s = 0
+        m += 1
+    timer_tag.config(text=f"{m}:{s}")
+    timer = window.after(1000, timer_count, m, s+1)
+
+
+timer_tag = Label(text="0:00", font=("Arial", 13, "bold"))
 timer_tag.pack()
 
-typing_box = Text(height=5, width=30)
+typing_box = Text(height=8, width=80, font=("Arial", 12))
 typing_box.focus()
 typed = typing_box.get("1.0", END)
 typing_box.pack()
 
 
 def start_timer():
-    paragraph.config(text=choice(sentences))
+    global start
+    if start:
+        paragraph.config(text=textwrap.fill(choice(sentences), width=80))
+        start_button.config(text="Stop")
+        timer_count(0, 0)
+        start = False
+    else:
+        start_button.config(text="Start")
+        window.after_cancel(timer)
+        timer_tag.config(text=f"Congrats!\nYour time is {timer_tag.cget('text')}")
+        start = True
 
 
 start_button = Button(text="Start", command=start_timer)
